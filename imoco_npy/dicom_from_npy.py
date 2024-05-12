d74d56431828c80c152bf78c03e4d3db8f6d2e59
njstewart-eju <zephyr853@gmail.com>
2024-05-12 15:49:15 +0100

    revert dicom function

--- imoco_npy/dicom_from_npy.py
+++ imoco_npy/dicom_from_npy.py
@@ -1,7 +1,9 @@
+#!/usr/bin/env python3
 """
-Conversion of numpy array output of imoco recon to DICOM series - adapted from imoco_py/dicom_creation.py
+Conversion of numpy array output of imoco/mocolor recon to DICOM series - adapted from imoco_py/dicom_creation.py
+https://github.com/PulmonaryMRI/imoco_recon/blob/master/imoco_py/dicom_creation.py
 Command line inputs needed: raw data (numpy array) directory; original DICOM directory
-Modified by: Neil J Stewart njstewart-eju (2023/04)
+Modified by: Neil J Stewart njstewart-eju (2023/08)
 Original author: Fei Tan (ftan1)
 """
 import numpy as np
@@ -10,20 +12,32 @@ import datetime
 import os
 import glob
 import sys
+import argparse
+import logging
 
-if __name__ == '__main__':
+if __name__=='__main__':
+    # parse arguments
+    parser = argparse.ArgumentParser(
+         description='Convert numpy array to DICOM')
+    
+    parser.add_argument('input_file', type=str, help='path to numpy file')
+    parser.add_argument('orig_dicom_dir', type=str, help='path to DICOM directory')
+    parser.add_argument('--flip_lr_flag', type=int, default=1, help='flag to flip the images')
+    parser.add_argument('--flip_ud_flag', type=int, default=1, help='flag to flip the images')
+    args = parser.parse_args()
+
+    flip_lr_flag = args.flip_lr_flag
+    flip_ud_flag = args.flip_ud_flag
+    input_file = args.input_file
+    orig_dicom_dir = args.orig_dicom_dir
+    logging.basicConfig(level=logging.INFO)
+
     # set directories
-    npy_dir = sys.argv[1]
-    orig_dicom_dir = sys.argv[2]
-    new_dicom_dir = os.path.join(npy_dir, 'imoco_dcm')
-
-    # load data
-    im = np.load(os.path.join(npy_dir, 'imoco.npy'))
+    im = np.load(input_file)
+    
     im = np.transpose(im, axes=[0, 2, 1])
-    #im = np.transpose(im[:, ::-1, ::-1], axes=[0, 2, 1])
-
-    # Uncomment this line to flipe images up-down
-    #im = np.flip(im,0)
+    
+    new_dicom_dir = input_file[:-4] + '_dcm'
 
     # load original dicom
     # exam number, series number
@@ -35,13 +49,11 @@ if __name__ == '__main__':
     exam_number, series_mimic, _ = dcm_file.replace('Exam', '').replace(
         'Series', '_').replace('Image', '_').replace('.dcm', '').split('_')
     exam_number = int(exam_number)
-    # adding 10, this should ensure no overlap with other series numbers for Philips numbering which uses series number (in order acquired) *100
-    series_write = int(series_mimic) + 10
 
-    # modified time
-    dt = datetime.datetime.now()
+    im_shape = np.shape(im)
+    ds.Columns, ds.Rows = im_shape[-2], im_shape[-1]
 
-    ds.SeriesDescription = "3D UTE iMoCo"
+    spatial_resolution = ds.SliceThickness
 
     # Update SliceLocation information
     series_mimic_slices = np.double(ds.Columns)  # assume recon is isotropic
@@ -49,10 +61,6 @@ if __name__ == '__main__':
         (series_mimic_slices - 1) / 2 * ds.SpacingBetweenSlices
     ImagePosition_zcenter = ds.ImagePositionPatient[2] + (
         series_mimic_slices - 1) / 2 * ds.SpacingBetweenSlices
-
-    im_shape = np.shape(im)
-    ds.Columns, ds.Rows = im_shape[-2], im_shape[-1]
-    spatial_resolution = ds.SliceThickness
 
     ds.SpacingBetweenSlices = spatial_resolution
     ds.PixelSpacing = [spatial_resolution, spatial_resolution]
@@ -62,10 +70,23 @@ if __name__ == '__main__':
     SliceLocation_original = ds.SliceLocation
     ImagePositionPatient_original = ds.ImagePositionPatient
 
+    # Comment this line to flip images up-down
+    if flip_ud_flag:
+        im = np.flip(im,0)
+    if flip_lr_flag:
+        im = np.flip(im,2)
+
     try:
         os.mkdir(new_dicom_dir)
     except OSError as error:
         pass
+
+    ds.SeriesDescription = '3D UTE - imoco' 
+    # adding 10, this should ensure no overlap with other series numbers for Philips numbering which uses series number (in order acquired) *100
+    series_write = int(series_mimic) + (10*(ph+1))
+
+    # modified time
+    dt = datetime.datetime.now()
 
     im = np.abs(im) / np.amax(np.abs(im)) * 4095  # 65535
     im = im.astype(np.uint16)
@@ -78,7 +99,7 @@ if __name__ == '__main__':
     ds.SeriesInstanceUID = pyd.uid.generate_uid()
 
     # not currently accounting for oblique slices...
-    for z in range(im_shape[0]):
+    for z in range(ds.Rows):
         ds.InstanceNumber = z + 1
         ds.SeriesNumber = series_write
         ds.SOPInstanceUID = pyd.uid.generate_uid()
@@ -87,9 +108,9 @@ if __name__ == '__main__':
         Filename = '{:s}/E{:d}S{:d}I{:d}.DCM'.format(
             new_dicom_dir, exam_number, series_write, z + 1)
         ds.SliceLocation = SliceLocation_original + \
-            (im_shape[0] / 2 - (z + 1)) * spatial_resolution
+            (im_shape[-1] / 2 - (z + 1)) * spatial_resolution
         ds.ImagePositionPatient = pyd.multival.MultiValue(float, [float(ImagePositionPatient_original[0]), float(
-            ImagePositionPatient_original[1]), ImagePositionPatient_original[2] - (im_shape[0] / 2 - (z + 1)) * spatial_resolution])
+            ImagePositionPatient_original[1]), ImagePositionPatient_original[2] - (im_shape[-1] / 2 - (z + 1)) * spatial_resolution])
         b = im[z, :, :].astype('<u2')
         ds.PixelData = b.T.tobytes()
         #ds.is_little_endian = False
